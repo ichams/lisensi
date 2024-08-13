0ed3843c3199b9ce2b11f116f28a74f1b43600ce
ichams <store.rumahsehatherbal@gmail.com>
2024-08-13 23:14:30 +0700

    Add files via upload

---
+++ lisensi.js
@@ -0,0 +1,19 @@
+function checkUserLicense(licenseKey) {
+  var url = "https://script.google.com/macros/s/AKfycbxwT4wiXaZFeCpjYpCqAkUR7bv7JQqOqpZ-ZVU7QLiSAsu5BzzvwecaHxH6tVTF928b/exec?key=" + licenseKey;
+  const Lisensi = atob("aHR0cHM6Ly9iaGFyYXRhaW50ZXJuYXNpb25hbC5zdG9yZQ==");
+  fetch(url)
+    .then(response => response.text())
+    .then(result => {
+      if (result.trim() === "VALID") {
+      } else {
+        window.location.href = Lisensi; 
+      }
+    })
+    .catch(error => {
+      window.location.href = Lisensi; 
+    });
+}
+
+window.onload = function() {
+  checkUserLicense(licenseKey);
+};
